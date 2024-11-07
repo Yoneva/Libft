675cedb75747c4686dcbd2d74d1906f73f9a70af
Amal Sbai <amsbai@c1r5p1.1337.ma>
2024-11-07 23:22:38 +0100

    libft

---
+++ ft_strmapi.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amsbai <amsbai@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/29 22:58:07 by amsbai            #+#    #+#             */
+/*   Updated: 2024/11/02 17:18:55 by amsbai           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char			*str;
+	unsigned int	i;
+	unsigned int	slen;
+
+	slen = ft_strlen(s);
+	i = 0;
+	str = malloc(slen + 1);
+	if (!s || !f)
+		return (NULL);
+	if (!str)
+		return (NULL);
+	while (s[i] != '\0')
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = 0;
+	return (str);
+}
